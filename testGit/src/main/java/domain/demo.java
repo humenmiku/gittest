a9f60a975920f8d128eda667f9b179e680e0ea59
humenmiku <71753428+humenmiku@users.noreply.github.com>
2020-11-23 14:56:05 +0800

    Update demo.java
    update

--- testGit/src/main/java/domain/demo.java
+++ testGit/src/main/java/domain/demo.java
@@ -4,5 +4,6 @@ public class demo {
     public static void main(String[] args) {
         System.out.println("主干");
         System.out.println("dev分支");
+         System.out.println("在线更新");
     }
 }
